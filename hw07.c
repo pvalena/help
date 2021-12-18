f0a65116a5eb3cc2752de65bf669302b2bb5950c
Pavel Valena <pvalena@redhat.com>
2021-12-18 21:11:45 +0100

    sync

--- hw07.c
+++ hw07.c
@@ -6,7 +6,12 @@
 #define WIDTH 368
 #define HEIGHT 448
 #define TEST 0
-#define TEST1 1
+#define TEST1 0
+
+#define FRAME 15
+#define WIDTHDIG 154
+#define WIDTHRECT 30
+#define HEIGHTDIG 195
 
 typedef unsigned char  byte;
 
@@ -90,20 +95,15 @@ int to1D(int row, int col, int cols){
 
 //SET ALL 4 ATRIBUTES OF SINGLE PIXEL
 void set_pixel(TGAImage *self, int x, int y, RGBA *pix){
-    int height = tga_height(self);
-    self->data[to1D(y,x,height)] = *pix; //plocha kopie, jde pac nejsou ukazatel
-
-    /*self->data[to1D(y,x, tga_height(self))].alpha = pix->alpha;
-    self->data[to1D(y,x, tga_height(self))].blue = pix->blue;
-    self->data[to1D(y,x, tga_height(self))].green = pix->green;
-    self->data[to1D(y,x, tga_height(self))].red = pix->red;*/
+    int width = tga_width(self);
+    self->data[to1D(x,y,width)] = *pix; //plocha kopie, jde pac nejsou ukazatel
 }
 
 
 
 
 //DRAWING FUNCTIONS
-//background
+//background ....can set pixels on 0 same way as memset does
 void draw_bg(TGAImage *self, RGBA *bg){
     int height = tga_height(self);
     int width = tga_width(self);
@@ -119,38 +119,101 @@ void draw_bg(TGAImage *self, RGBA *bg){
 
 }
 
-//digits
-void draw_one(TGAImage *self, int dx, int dy, RGBA *pix){
-    for (int i = 0 + dx; i < 195 + dx; i++)
+//MAIN DRAW FUNCTION ..rectangle shape
+void draw(TGAImage *self, int x_start, int x_end, int y_start, int y_end, RGBA *pix){
+    for (int i = x_start; i < x_end; i++)
     {
-        for (int j = 0 + dx; j < 169 + dy; j++)
+        for (int j = y_start; j < y_end; j++)
         {
-            if (i > 15 + dx && j > 139 + dy)
-            {
-                set_pixel(self, j, i, pix);
-                if (TEST1) printf("one: i:%d j:%d\n",i ,j);
-            }
-        }
-    }
-}
-
-//zatim dela to stejne co draw_one ()
-void draw_zero(TGAImage *self, int dx, int dy, RGBA *pix){
-    for (int i = 0 + dx; i < 195 + dx; i++)
-    {
-        for (int j = 0 + dx; j < 169 + dy; j++)
-        {
-            if (i > 15 + dx && j > 139 + dy)
-            {
                 set_pixel(self, i, j, pix);
-                printf("one: i:%d j:%d\n",i ,j);
-            }
+                if (TEST) printf("one: i:%d j:%d\n",i ,j);
         }
     }
 }
 
 
-//TASK functions
+//digits combined from rectangles
+void draw_zero(TGAImage *self, int dx, int dy, RGBA *pix){
+    draw (self, dx + FRAME, dx + FRAME + HEIGHTDIG, dy +  FRAME, dy + FRAME + WIDTHRECT , pix);  //|..
+    draw (self, dx + FRAME, dx + FRAME + HEIGHTDIG, dy +  FRAME + WIDTHDIG - WIDTHRECT, dy + FRAME + WIDTHDIG , pix); //..|
+    draw (self, dx + FRAME, dx + FRAME + WIDTHRECT, dy +  FRAME + WIDTHRECT, dy + FRAME + WIDTHDIG , pix); //-
+    draw (self, dx + FRAME +HEIGHTDIG - WIDTHRECT, dx + FRAME + HEIGHTDIG, dy +  FRAME + WIDTHRECT, dy + FRAME + WIDTHDIG , pix); //_
+
+}
+
+void draw_one(TGAImage *self, int dx, int dy, RGBA *pix){
+     draw (self, dx + FRAME, dx + FRAME + HEIGHTDIG, dy +  FRAME + WIDTHDIG - WIDTHRECT, dy + FRAME + WIDTHDIG , pix); //..|
+}
+
+void draw_two(TGAImage *self, int dx, int dy, RGBA *pix){
+    draw (self, dx + FRAME + HEIGHTDIG/2, dx + FRAME + HEIGHTDIG, dy +  FRAME, dy + FRAME + WIDTHRECT , pix);  //|..
+    draw (self, dx + FRAME, dx + FRAME + HEIGHTDIG/2, dy +  FRAME + WIDTHDIG - WIDTHRECT, dy + FRAME + WIDTHDIG , pix); //..|
+    draw (self, dx + FRAME  + HEIGHTDIG/2 - WIDTHRECT/2, dx + FRAME  + HEIGHTDIG/2 -WIDTHRECT/2 + WIDTHRECT, dy +  FRAME , dy + FRAME + WIDTHDIG , pix); //---
+    draw (self, dx + FRAME, dx + FRAME + WIDTHRECT, dy +  FRAME , dy + FRAME + WIDTHDIG , pix); //-
+    draw (self, dx + FRAME +HEIGHTDIG - WIDTHRECT, dx + FRAME + HEIGHTDIG, dy +  FRAME + WIDTHRECT, dy + FRAME + WIDTHDIG , pix); //_
+
+}
+
+void draw_three(TGAImage *self, int dx, int dy, RGBA *pix){
+  
+    draw (self, dx + FRAME, dx + FRAME + HEIGHTDIG, dy +  FRAME + WIDTHDIG - WIDTHRECT, dy + FRAME + WIDTHDIG , pix);
+    draw (self, dx + FRAME  + HEIGHTDIG/2 - WIDTHRECT/2, dx + FRAME  + HEIGHTDIG/2 -WIDTHRECT/2 + WIDTHRECT, dy +  FRAME , dy + FRAME + WIDTHDIG , pix); //---
+    draw (self, dx + FRAME, dx + FRAME + WIDTHRECT, dy +  FRAME , dy + FRAME + WIDTHDIG , pix); //-
+    draw (self, dx + FRAME +HEIGHTDIG - WIDTHRECT, dx + FRAME + HEIGHTDIG, dy +  FRAME , dy + FRAME + WIDTHDIG , pix); //_
+
+}
+
+void draw_foure(TGAImage *self, int dx, int dy, RGBA *pix){
+    draw (self, dx + FRAME, dx + FRAME + HEIGHTDIG/2, dy +  FRAME, dy + FRAME + WIDTHRECT , pix);  //|..
+    draw (self, dx + FRAME, dx + FRAME + HEIGHTDIG, dy +  FRAME + WIDTHDIG - WIDTHRECT, dy + FRAME + WIDTHDIG , pix);//...|
+    draw (self, dx + FRAME  + HEIGHTDIG/2 - WIDTHRECT/2, dx + FRAME  + HEIGHTDIG/2 -WIDTHRECT/2 + WIDTHRECT, dy +  FRAME , dy + FRAME + WIDTHDIG , pix); //---
+}
+
+void draw_five(TGAImage *self, int dx, int dy, RGBA *pix){
+  
+    draw (self, dx + FRAME, dx + FRAME + HEIGHTDIG/2, dy +  FRAME, dy + FRAME + WIDTHRECT , pix);  //|..
+    draw (self, dx + FRAME + HEIGHTDIG/2, dx + FRAME + HEIGHTDIG, dy +  FRAME + WIDTHDIG - WIDTHRECT, dy + FRAME + WIDTHDIG , pix); //..|
+    draw (self, dx + FRAME  + HEIGHTDIG/2 - WIDTHRECT/2, dx + FRAME  + HEIGHTDIG/2 -WIDTHRECT/2 + WIDTHRECT, dy +  FRAME , dy + FRAME + WIDTHDIG , pix); //---
+    draw (self, dx + FRAME, dx + FRAME + WIDTHRECT, dy +  FRAME , dy + FRAME + WIDTHDIG , pix); //-
+    draw (self, dx + FRAME +HEIGHTDIG - WIDTHRECT, dx + FRAME + HEIGHTDIG, dy +  FRAME, dy + FRAME + WIDTHDIG , pix); //_
+}
+
+void draw_six(TGAImage *self, int dx, int dy, RGBA *pix){
+  
+    draw (self, dx + FRAME, dx + FRAME + HEIGHTDIG, dy +  FRAME, dy + FRAME + WIDTHRECT , pix);  //|..
+    draw (self, dx + FRAME + HEIGHTDIG/2, dx + FRAME + HEIGHTDIG, dy +  FRAME + WIDTHDIG - WIDTHRECT, dy + FRAME + WIDTHDIG , pix); //..|
+    draw (self, dx + FRAME  + HEIGHTDIG/2 - WIDTHRECT/2, dx + FRAME  + HEIGHTDIG/2 -WIDTHRECT/2 + WIDTHRECT, dy +  FRAME , dy + FRAME + WIDTHDIG , pix); //---
+    draw (self, dx + FRAME, dx + FRAME + WIDTHRECT, dy +  FRAME , dy + FRAME + WIDTHDIG , pix); //-
+    draw (self, dx + FRAME +HEIGHTDIG - WIDTHRECT, dx + FRAME + HEIGHTDIG, dy +  FRAME, dy + FRAME + WIDTHDIG , pix); //_
+}
+
+void draw_seven(TGAImage *self, int dx, int dy, RGBA *pix){
+    draw (self, dx + FRAME, dx + FRAME + WIDTHRECT, dy +  FRAME , dy + FRAME + WIDTHDIG , pix); //-
+    draw (self, dx + FRAME, dx + FRAME + HEIGHTDIG, dy +  FRAME + WIDTHDIG - WIDTHRECT, dy + FRAME + WIDTHDIG , pix); //..|
+}
+
+void draw_eight(TGAImage *self, int dx, int dy, RGBA *pix){
+
+    draw (self, dx + FRAME, dx + FRAME + HEIGHTDIG, dy +  FRAME, dy + FRAME + WIDTHRECT , pix);  //|..
+    draw (self, dx + FRAME, dx + FRAME + HEIGHTDIG, dy +  FRAME + WIDTHDIG - WIDTHRECT, dy + FRAME + WIDTHDIG , pix);
+    draw (self, dx + FRAME  + HEIGHTDIG/2 - WIDTHRECT/2, dx + FRAME  + HEIGHTDIG/2 -WIDTHRECT/2 + WIDTHRECT, dy +  FRAME , dy + FRAME + WIDTHDIG , pix); //---
+    draw (self, dx + FRAME, dx + FRAME + WIDTHRECT, dy +  FRAME , dy + FRAME + WIDTHDIG , pix); //-
+    draw (self, dx + FRAME +HEIGHTDIG - WIDTHRECT, dx + FRAME + HEIGHTDIG, dy +  FRAME , dy + FRAME + WIDTHDIG , pix); //_
+
+}
+
+void draw_nine(TGAImage *self, int dx, int dy, RGBA *pix){
+    draw (self, dx + FRAME, dx + FRAME + HEIGHTDIG/2, dy +  FRAME, dy + FRAME + WIDTHRECT , pix);  //|.. 
+    draw (self, dx + FRAME, dx + FRAME + HEIGHTDIG, dy +  FRAME + WIDTHDIG - WIDTHRECT, dy + FRAME + WIDTHDIG , pix); //...|
+    draw (self, dx + FRAME  + HEIGHTDIG/2 - WIDTHRECT/2, dx + FRAME  + HEIGHTDIG/2 -WIDTHRECT/2 + WIDTHRECT, dy +  FRAME , dy + FRAME + WIDTHDIG , pix); //---
+    draw (self, dx + FRAME, dx + FRAME + WIDTHRECT, dy +  FRAME , dy + FRAME + WIDTHDIG , pix); //-
+    draw (self, dx + FRAME +HEIGHTDIG - WIDTHRECT, dx + FRAME + HEIGHTDIG, dy +  FRAME , dy + FRAME + WIDTHDIG , pix); //_
+
+}
+
+
+
+//TASK function 1
 void watch_draw_time(TGAImage* self, const int hours, const int minutes){
 
     FILE *file = fopen("aw.tga","wb");
@@ -162,21 +225,66 @@ void watch_draw_time(TGAImage* self, const int hours, const int minutes){
     int firstMin = minutes / 10;
     int secondMin = minutes % 10;
 
-    //pixel definition for foreground
-    RGBA fg = {.red = 30, .green = 10, .blue = 10, .alpha = 255};
-    //set_pixel(self,0,0,&fg);
+  
+    //pixel definition for foreground and background
+    RGBA fg = {.red = 100, .green = 10, .blue = 10, .alpha = 255};
+    RGBA bg = {.red = 220, .green = 220, .blue = 20, .alpha = 255};
+    //popuilated backround color
+    draw_bg(self, &bg); 
 
-    //FUNCTION POINTERS ARRAY (prvni prvek func_array[0] = &draw zero, druhy prvke array[1] = &draw one....)
+
+
+    //FUNCTION POINTERS ARRAY populated by draw function - indexing corresponds with digits ......(prvni prvek func_array[0] = &draw zero, druhy prvke array[1] = &draw one....)
     void (*func_array[])(TGAImage*, int, int, RGBA*) = {
-        draw_zero, draw_one, /*draw_two, draw_three, draw_foure, draw_five, draw_six, draw seven, draw_eight, draw_nine*/
+        draw_zero, draw_one, draw_two, draw_three, draw_foure, draw_five, draw_six, draw_seven, draw_eight, draw_nine
     };
 
 
     //function calling based on individual digit (dx, dy prirustky podle toho kera/kde je cislice na ciferniku)
-    func_array[firstH](self, 0, 0, &fg);
-  //  func_array[secondH](self, 184, 0, &fg);
-  //  func_array[firstMin](self, 0, 224, &fg);
-  //  func_array[secondMin](self, 184, 224, &fg);  //uncomment az bude fungovat
+    func_array[firstH](self, 0 , 0, &fg);
+    func_array[secondH](self, 0 ,2*FRAME + WIDTHDIG, &fg);
+    func_array[firstMin](self, 2*FRAME + HEIGHTDIG, 0, &fg);
+    func_array[secondMin](self, 2*FRAME + HEIGHTDIG, 2*FRAME + WIDTHDIG, &fg);  
+
+    //write it all in the file
+    int writeCount = fwrite( &self->header, sizeof (TGAheader),1,file); //tady adresu &self->header, pack je to primo clen ("hodnota")
+    assert(writeCount == 1);
+
+    writeCount = fwrite(self->data, sizeof(RGBA), tga_width(self) * tga_height(self),file); //tady NE adresu &self primo ten clen je vlastne ukazatel na misto co chci zapisovat 
+
+    fclose(file);
+}
+
+//TASK function 2 with pixels parameters
+void watch_draw_time_color(TGAImage* self, const int hours, const int minutes, const RGBA* fg_color, const RGBA* bg_color){
+
+    FILE *file = fopen("aw.tga","wb");
+    assert(file);
+
+    //boil down to single digits
+    int firstH = hours / 10;;
+    int secondH = hours % 10;
+    int firstMin = minutes / 10;
+    int secondMin = minutes % 10;
+
+    //local copies since const param.
+    RGBA fgLoc = (*fg_color);
+    RGBA bgLoc = (*bg_color);
+    
+    //popuilated backround color
+    draw_bg(self, &bgLoc);
+
+
+    //FUNCTION POINTERS ARRAY populated by draw function - indexing corresponds with digits ......(prvni prvek func_array[0] = &draw zero, druhy prvke array[1] = &draw one....)
+    void (*func_array[])(TGAImage*, int, int, RGBA*) = {
+        draw_zero, draw_one, draw_two, draw_three, draw_foure, draw_five, draw_six, draw_seven, draw_eight, draw_nine
+    };
+
+    //function calling based on individual digit (dx, dy prirustky podle toho kera/kde je cislice na ciferniku)
+    func_array[firstH](self, 0 , 0, &fgLoc);
+    func_array[secondH](self, 0 ,2*FRAME + WIDTHDIG, &fgLoc);
+    func_array[firstMin](self, 2*FRAME + HEIGHTDIG, 0, &fgLoc);
+    func_array[secondMin](self, 2*FRAME + HEIGHTDIG, 2*FRAME + WIDTHDIG, &fgLoc);  //uncomment az bude fungovat
 
     //write it all in the file
     int writeCount = fwrite( &self->header, sizeof (TGAheader),1,file); //tady adresu &self->header, pack je to primo clen ("hodnota")
@@ -188,7 +296,8 @@ void watch_draw_time(TGAImage* self, const int hours, const int minutes){
 }
 
 
-//tady vytvarim vlastni struktury a nastavuju jejich hodnoty > PRAVDEPODOBNE PROBLE ZDE NEKDE
+
+//function to create new TGA_Image STRUCT
 TGAImage *tga_new(const int height, const int width){
     TGAImage *tga = (TGAImage*)malloc(sizeof(TGAImage));// heap allocation for the whole image
  
@@ -224,39 +333,34 @@ void tga_free(TGAImage **self){
     (*self)->data = NULL;
     free(*self);
     *self = NULL;
-
 }
+
 
 
 int main(int argc, char **argv){
     int hours = 0;
     int minutes = 0;
 
-    if (argc != 3)
-    {
+    if (argc != 3){
+    
         return 1;
     }
-    else
-    {
-        hours = atoi(argv[1]);
-        minutes = atoi(argv[2]);
-    }
-
+    
+    hours = atoi(argv[1]);
+    minutes = atoi(argv[2]);
     
 
     //vytvorim strukturu obrazku
     TGAImage *self = tga_new(HEIGHT,WIDTH);
 
  
-    //pixel setting for foreground and background
-    RGBA bg = {.red = 220, .green = 220, .blue = 20, .alpha = 255};
-    RGBA fg = {.red = 50, .green = 10, .blue = 50, .alpha = 255};
+    //pixel setting for foreground and background for watch_draw_time_color function
+    RGBA bg = {.red = 220, .green = 220, .blue = 200, .alpha = 255};
+    RGBA fg = {.red = 250, .green = 10, .blue = 50, .alpha = 255};
 
     
-    draw_bg(self, &bg);
-
-
     watch_draw_time(self,hours, minutes);
+    watch_draw_time_color(self, hours, minutes, &fg, &bg);
 
 
     tga_free(&self); //fre asi potrebuje adresu self aby z fce mohla prepsat ukazatel self samotny na NULL a vypropagovat zmenu ven z funkce takhle
